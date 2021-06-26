b9c827ee68a76f690d41b6809a2f1d89138299c9
Grzechu <grzegorz.jamrooz@gmail.com>
2021-06-26 09:42:57 +0200

    added boot method

--- src/PageSourceSystemBundle.php
+++ src/PageSourceSystemBundle.php
@@ -7,16 +7,16 @@ namespace Ifrost\Bundle\PageSourceSystemBundle;
 use Ifrost\Bundle\PageSourceSystemBundle\Utilities\StorageInitiator;
 use Ifrost\PageSourceComponents\SettingCollection;
 use PageSourceSystem\Repository\SettingsRepository;
+use PageSourceSystem\Setting\BaseGeneral;
+use PageSourceSystem\Setting\BaseLanguages;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class PageSourceSystemBundle extends Bundle
 {
-    public function build(ContainerBuilder $container): void
+    public function boot()
     {
-        parent::build($container);
-
-        $directory = $container->getParameter('page_source_system.app_data_dir');
+        $directory = $this->container->getParameter('page_source_system.app_data_dir');
 
         if (!is_string($directory)) {
             throw new \Exception('Parameter page_source_system.app_data_dir is not set as string.');
@@ -24,8 +24,16 @@ class PageSourceSystemBundle extends Bundle
 
         (new StorageInitiator(
             $directory,
-            new SettingsRepository($directory, new SettingCollection()),
+            new SettingsRepository($directory, new SettingCollection([
+                BaseLanguages::getTypename() => BaseLanguages::createFromArray([]),
+                BaseGeneral::getTypename() => BaseGeneral::createFromArray([]),
+            ])),
         ))->init();
+    }
+
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
     }
 
     public function getPath(): string
